4607e5c3c956dd1ea50c13826d02d996babfce44
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-24 14:20:57 -0500

    Inicializando solución c#

---
+++ ArraysMultiDimensionales/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArraysMultiDimensionales
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //declaración de array de dos dimensiones
+            string[,] matriz2D;
+
+            //declaración de array de tres dimensiones
+            int[,,] matriz3D;
+
+            string[,] array2D = new string[,]
+            {
+                {"A","B","C" },
+                {"D","E","F" },
+                {"G","H","I" }
+            };
+            Console.WriteLine("El valor central es {0}", array2D[1,1]);
+            Console.Read();
+        }
+    }
+}
