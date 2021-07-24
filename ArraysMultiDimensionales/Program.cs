dea4400f39f0acf4364bf0736a6dee186283ed02
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-24 14:41:13 -0500

    ArraysMultiDimensionales

--- ArraysMultiDimensionales/Program.cs
+++ ArraysMultiDimensionales/Program.cs
@@ -18,7 +18,36 @@ namespace ArraysMultiDimensionales
                 {"D","E","F" },
                 {"G","H","I" }
             };
-            Console.WriteLine("El valor central es {0}", array2D[1,1]);
+
+
+            string[,,] array3D = new string[,,]
+            {
+                {
+
+                {"A","B","C" },
+                {"D","E","F" },
+                {"G","H","I" }
+
+                },
+
+                {
+
+                {"J","K","L" },
+                {"M","N","O" },
+                {"P","Q","R" }
+                },
+
+                {
+
+                {"S","T","U" },
+                {"V","W","X" },
+                {"Y","Z", "*"}
+
+                }
+            };
+            Console.WriteLine("El valor central es {0}", array2D[2,0]);
+            Console.WriteLine("El valor de Q es {0}", array3D[1,2,1]);
+            Console.WriteLine("El valor de V es {0}", array3D[2,1,0]);
             Console.Read();
         }
     }
